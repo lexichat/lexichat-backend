b8a482db06921d7f0c9b439af43c0a9e1a0ae123
Shripad Rao <shripada0987@gmail.com>
2024-03-30 19:58:54 +0530

    change datatype for phone no

--- scripts/init.sql
+++ scripts/init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     username VARCHAR(255),
-    phone_number INT,
+    phone_number TEXT,
     profile_picture BYTEA,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
