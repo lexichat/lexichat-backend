0559259422fdfe0f4fc85e4a9fdfce1848114e29
Shripad Rao <shripada0987@gmail.com>
2024-04-05 00:52:04 +0530

    add global config file

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,17 +1,35 @@
 package config
 
 import (
+	"log"
 	"os"
 
-	_ "github.com/joho/godotenv/autoload"
+	"github.com/joho/godotenv"
 )
 
 var (
-    DBHost     = os.Getenv("POSTGRES_HOST")
-    DBPort     = os.Getenv("POSTGRES_PORT")
-    DBUser     = os.Getenv("POSTGRES_USER")
-    DBPassword = os.Getenv("POSTGRES_PASSWORD")
-    DBName     = os.Getenv("POSTGRES_DB")
-	GOLANG_SERVER_PORT = os.Getenv("GOLANG_SERVER_PORT")
-    JWT_SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
+	DBHost         string
+	DBPort         string
+	DBUser         string
+	DBPassword     string
+	DBName         string
+	APP_SERVER_PORT  string
+	WS_SERVER_PORT   string
+	JWT_SECRET_KEY   string
 )
+
+func LoadEnvVariables(envPath string) {
+	err := godotenv.Load(envPath)
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	DBHost = os.Getenv("POSTGRES_HOST")
+	DBPort = os.Getenv("POSTGRES_PORT")
+	DBUser = os.Getenv("POSTGRES_USER")
+	DBPassword = os.Getenv("POSTGRES_PASSWORD")
+	DBName = os.Getenv("POSTGRES_DB")
+	APP_SERVER_PORT = os.Getenv("APP_SERVER_PORT")
+	WS_SERVER_PORT = os.Getenv("WS_SERVER_PORT")
+	JWT_SECRET_KEY = os.Getenv("JWT_SECRET_KEY")
+}
